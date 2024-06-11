0c3f1a3fe05a19634029ac161cba995d9cff9da8
afmurillo97 <afmurillo97@gmail.com>
2024-06-11 18:27:54 -0500

    use React.cloneElement to pass props inside all TodoHeader component

--- src/TodoHeader/index.js
+++ src/TodoHeader/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-function TodoHeader({ children }) {
+function TodoHeader({ children, loading }) {
+
 	return (
 		<header>
-			{ children }
+			{ 
+				React.Children
+					.toArray(children)
+					.map(child =>React.cloneElement(child, { loading }) ) 
+			}
 		</header>
 	);
+	
 }
 
 export { TodoHeader };
