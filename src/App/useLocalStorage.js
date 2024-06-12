15ff5f81e5ffda5606e2a6cb1ba2c84c4b53ee65
afmurillo97 <afmurillo97@gmail.com>
2024-06-12 13:49:20 -0500

    Add synchronizedItem state to conditionally synchronize todos with useLocalStorage hook

--- src/App/useLocalStorage.js
+++ src/App/useLocalStorage.js
@@ -20,6 +20,7 @@ function useLocalStorage(itemName, initialValue) {
   const [item, setItem] = React.useState(initialValue);
   const [loading, setLoading] = React.useState(true);
   const [error, setError] = React.useState(false);
+  const [sincronizedItem, setSincronizedItem] = React.useState(true);
 
   React.useEffect(() => {
     setTimeout(() => {
@@ -38,24 +39,31 @@ function useLocalStorage(itemName, initialValue) {
         }
 
         setLoading(false);
+        setSincronizedItem(true);
           
       } catch (error) {
         setLoading(false);
         setError(true);
       }
     }, 2000);
-  }, []);
+  }, [sincronizedItem]);
 
   const saveItem = (newItem) => {
     localStorage.setItem(itemName, JSON.stringify(newItem));
     setItem(newItem);
   };
 
+  const sincronize = () => {
+    setLoading(true);
+    setSincronizedItem(false);
+  };
+
   return {
     item, 
     saveItem,
     loading,
-    error
+    error,
+    sincronize
   };
 
 }
