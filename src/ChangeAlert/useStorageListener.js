ca6d668568f50ff23d8debfa32c41aba911219db
afmurillo97 <afmurillo97@gmail.com>
2024-06-12 13:52:37 -0500

    Create a custom hook useStorageListener to synchronize data across different browser tabs

---
+++ src/ChangeAlert/useStorageListener.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function useStorageListener(sincronize) {
+	
+	const [storageChange, setStorageChange] = React.useState(false);
+
+	window.addEventListener('storage', (change) => {
+		if (change.key === 'TODOS_V1'){
+			console.log('Hubo cambios en todos');
+			setStorageChange(true);
+		}
+	})
+
+	const toggleShow = () => {
+		sincronize();
+		setStorageChange(false);
+	}
+
+	return {
+		show: storageChange,
+		toggleShow
+	};
+	
+}
+
+export { useStorageListener }
